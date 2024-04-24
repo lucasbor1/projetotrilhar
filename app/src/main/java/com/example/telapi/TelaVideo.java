e7ebf851636927d76be01121f93bbbbc9bc1493f
LucasSoares134 <soareslucas134@gmail.com>
2024-04-24 20:41:29 -0300

    inicio tela

--- app/src/main/java/com/example/telapi/TelaVideo.java
+++ app/src/main/java/com/example/telapi/TelaVideo.java
@@ -1,12 +1,10 @@
 package com.example.telapi;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import androidx.activity.EdgeToEdge;
 
 public class TelaVideo extends AppCompatActivity {
 
@@ -14,11 +12,25 @@ public class TelaVideo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_video);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_tela_video); // Correção aqui
+
+        // Configurando os WebViews
+        WebView webView = findViewById(R.id.webView);
+        String video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/bFLp81P4C-U?si=iyq03uCzWHyVpV5J\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+        webView.loadData(video, "text/html", "utf-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+
+        WebView webView2 = findViewById(R.id.webView2);
+        String video2 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/h0nTZWiC1Xs?si=1P4-mXKBBYNPM6VN\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+        webView2.loadData(video2, "text/html", "utf-8");
+        webView2.getSettings().setJavaScriptEnabled(true);
+        webView2.setWebChromeClient(new WebChromeClient());
+
+        WebView webView3 = findViewById(R.id.webView3);
+        String video3 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/sFNMEe8r11E?si=cz3bznSihan3nLmR\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+        webView3.loadData(video3, "text/html", "utf-8");
+        webView3.getSettings().setJavaScriptEnabled(true);
+        webView3.setWebChromeClient(new WebChromeClient());
     }
 }
